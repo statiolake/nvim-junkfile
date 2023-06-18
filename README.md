3fa75395094406c483fd6f657cbec08cb66a1fb7
statiolake <statiolake@gmail.com>
2023-06-18 15:29:57 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nvim-junkfile
+
+Very simple plugin to open anonymous junk file. Lua version of [vim-junkfile](https://github.com/statiolake/vim-junkfile).
---
+++ lua/junkfile.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+function M.setup(opts)
+  M.opts = opts
+end
+
+local function get_workspace_path()
+  local path = M.opts.workspace_path or vim.fn.strftime '~/junk/%Y/%m%d'
+  return vim.fn.expand(path)
+end
+
+function M.make_and_edit_temp(ext)
+  local workdir = M.get_workspace_path()
+  if vim.fn.empty(workdir) then
+    vim.api.nvim_echo({
+      {
+        'nvim-junkfile: Workspace path is not configured',
+        'ErrorMsg',
+      },
+    }, true)
+    return
+  end
+
+  local filename = string.format('junk_%s.%s', vim.fn.strftime '%H%M%S', ext)
+  local path = vim.fn.expand(string.format('%s/%s', workdir, filename))
+
+  -- Edit within the current buffer if current buffer is empty. Open new tab
+  -- otherwise.
+  local edit_command
+  if vim.fn.expand '%' == '' and vim.opt.filetype == '' then
+    edit_command = 'edit'
+  else
+    edit_command = 'tabnew'
+  end
+
+  vim.cmd[edit_command](path)
+end
+
+return M
